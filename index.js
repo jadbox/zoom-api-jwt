04494a2ba0738f2f38ad34e6bdb3158bc22007f9
Jonathan Dunlap <jdunlap@outlook.com>
2020-03-30 21:37:45 -0700

    working create meeting

--- index.js
+++ index.js
@@ -11,15 +11,61 @@ var email, userid, resp;
 const port = 3000;
 
 //Use the ApiKey and APISecret from config.js
-const payload = {
-    iss: config.APIKey,
-    exp: ((new Date()).getTime() + 5000)
-};
-const token = jwt.sign(payload, config.APISecret);
+function makeToken() {
+    const payload = {
+        iss: config.APIKey,
+        exp: ((new Date()).getTime() + 5000)
+    };
+    const token = jwt.sign(payload, config.APISecret);
+    return token;
+}
 
 
 //get the form 
 app.get('/', (req,res) => res.send(req.body));
+
+app.post('/make', (req, res) => {
+    const token = makeToken();
+    const email = 'me'; // req.body.email;
+  //check if the email was stored in the console
+  console.log('/make');
+  //Store the options for Zoom API which will be used to make an API call later.
+  var options = {
+    method: 'POST',
+    body: {
+    },
+    //You can use a different uri if you're making an API call to a different Zoom endpoint.
+    uri: "https://api.zoom.us/v2/users/"+email+"/meetings", 
+    qs: {
+        status: 'active' 
+    },
+    auth: {
+        'bearer': token
+    },
+    headers: {
+        'User-Agent': 'Zoom-api-Jwt-Request',
+        'content-type': 'application/json'
+    },
+    json: true //Parse the JSON string in the response
+};
+
+//Use request-promise module's .then() method to make request calls.
+rp(options)
+    .then(function (response) {
+      //printing the response on the console
+        console.log('User has', response);
+        //console.log(typeof response);
+        resp = response
+        res.send(resp);
+ 
+    })
+    .catch(function (err) {
+        // API call failed...
+        console.log('API call failed, reason ', err);
+    });
+
+
+});
 
 //use userinfo from the form and make a post request to /userinfo
 app.post('/userinfo', (req, res) => {
